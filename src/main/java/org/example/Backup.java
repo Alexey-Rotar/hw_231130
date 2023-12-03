2f6f3a31185bb4836b8aca706edcb0844cdbbd3a
alexey.rotar <ar_mail@inbox.ru>
2023-12-03 18:52:00 +0500

    v1.2

---
+++ src/main/java/org/example/Backup.java
@@ -0,0 +1,73 @@
+package org.example;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Backup {
+    /**
+     * Метод создания директории
+     * @param path - путь для создания директории
+     */
+    private static void createDir(String path){
+        File dir = new File(path);
+        dir.mkdirs();
+    }
+
+    /**
+     * Метод побитового копирования файла
+     * @param fileIn - путь к исходному (копируемому) файлу
+     * @param fileOut - путь к файлу назначения
+     * @throws IOException
+     */
+    private static void copyFile(String fileIn, String fileOut) throws IOException {
+        // На запись
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)){
+            int c;
+            // На чтение
+            try (FileInputStream fileInputStream = new FileInputStream(fileIn)){
+                while ((c = fileInputStream.read()) != -1){
+                    fileOutputStream.write(c);
+                }
+            }
+        }
+    }
+
+    /**
+     * Рекурсивный внутренний private метод резервного копирования файлов
+     * @param file - исходная директория (объект типа File)
+     * @param toDirPath - путь к директории назначения (String)
+     * @param backupDirName - имя директории назначения (String)
+     * @throws IOException
+     */
+    private static void backupRecursive(File file, String toDirPath, String backupDirName) throws IOException {
+        File[] files = file.listFiles();
+        if (files == null)
+            return;
+
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()){
+                if (files[i].getName().equals(backupDirName))
+                    continue;
+                createDir(toDirPath + "/" + backupDirName + "/" + files[i].getPath());
+                backupRecursive(files[i], toDirPath, backupDirName);
+            }
+            if (files[i].isFile()) {
+                copyFile(files[i].toString(), toDirPath + "/" + backupDirName + files[i].getPath().substring(1));
+            }
+        }
+    }
+
+    /**
+     * Метод резервного копирования
+     * @param fromDirPath
+     * @param toDirPath
+     * @param backupDirName
+     * @throws IOException
+     */
+    public static void backup(String fromDirPath, String toDirPath, String backupDirName) throws IOException {
+        createDir(toDirPath + "/" + backupDirName);
+        backupRecursive(new File(fromDirPath), toDirPath, backupDirName);
+    }
+}
